ad22ab4f7295bcff4259138636e2d6f76d448ded
Nick Carchedi <nick.carchedi@gmail.com>
2013-08-31 16:35:14 -0400

    changed update configuration

--- R/swirl_MASTER.R
+++ R/swirl_MASTER.R
@@ -7,87 +7,89 @@
 #' @export
 #' @author Nicholas A. Carchedi
 swirl <- function() {
-  tryCatch({
+  # Prompt user to update swirl - returns TRUE if updating
+  updating <- promptUpdate()
+  if(!updating) {
     
-    # Prompt user to update swirl
-    updateSwirl()
-    
-    # Define user data directory path
-    userDataPath <- file.path(path.package("swirl"), "user_data")
-    
-    # Check if user_data directory exists and if not, create it
-    if(!file.exists(userDataPath)) {
-      dir.create(userDataPath)
-    }
-    
-    # Run openingMenu, which returns module name and row number on which to begin
-    cat("\nWelcome! My name is Swirl and I'll be your host today!")
-    start <- openingMenu()
-    module.start <- start[[1]]  # Character string
-    row.start <- start[[2]]  # Numeric
-    
-    # Set names of files where user info and progress can be found
-    files <- unlist(start[[3]])
-    user.info.file.name <- files[1]
-    progress.file.name <- files[2]
-    
-    course.start <- start[[4]][[1]]  # This is the course directory name
-    courseName <- start[[4]][[2]] # This is the actual name of the course
-    
-    ##### SET DIRECTORY WHERE MODULES OF INTEREST ARE LOCATED #####
-    course.dir <- file.path(path.package("swirl"), course.start)
-    
-    # Create master module list and get element number of starting module
-    modules <- dir(course.dir, pattern="[a-zA-Z]+[0-9]+\\.rda")
-    master.module.list <- gsub(".rda", "", as.list(modules))
-    mod.num <- which(master.module.list==module.start)
-    
-    # Start running modules beginning with starting module
-    for(i in mod.num:length(master.module.list)) {
-      # Run module i
-      module.name <- master.module.list[[i]]
-      runModule(courseDir=course.dir, module.name=module.name, 
-                row.start=row.start, progress.file.path=progress.file.name, 
-                courseName=courseName)
+    tryCatch({
       
-      # Suggest topics to review
-      if(file.exists(progress.file.name)) {
-        taggedTopics <- findTroubleTags(progressFilePath=progress.file.name)
-        if(!identical(taggedTopics, NA)) {
-          cat("\nIt appears that you struggled with the following topics:",
-              paste(taggedTopics, collapse=", "), "\n")
-          cat("\nWhich of these topics would you like to review?\n")
-          options <- c(taggedTopics, "I'm good to go!")
-          tags2Review <- select.list(options, multiple=TRUE)
-          
-          if(!identical(tags2Review, "I'm good to go!")) {
-            # Run module in review mode for tags of interest
-            runModule(courseDir=course.dir, module.name=module.name, 
-                      row.start=1, progress.file.path=progress.file.name,
-                      review=TRUE, tags=tags2Review)
-            cat("\nYou've completed your review for the topics you selected!\n")
+      # Define user data directory path
+      userDataPath <- file.path(path.package("swirl"), "user_data")
+      
+      # Check if user_data directory exists and if not, create it
+      if(!file.exists(userDataPath)) {
+        dir.create(userDataPath)
+      }
+      
+      # Run openingMenu, which returns module name and row number on which to begin
+      cat("\nWelcome! My name is Swirl and I'll be your host today!")
+      start <- openingMenu()
+      module.start <- start[[1]]  # Character string
+      row.start <- start[[2]]  # Numeric
+      
+      # Set names of files where user info and progress can be found
+      files <- unlist(start[[3]])
+      user.info.file.name <- files[1]
+      progress.file.name <- files[2]
+      
+      course.start <- start[[4]][[1]]  # This is the course directory name
+      courseName <- start[[4]][[2]] # This is the actual name of the course
+      
+      ##### SET DIRECTORY WHERE MODULES OF INTEREST ARE LOCATED #####
+      course.dir <- file.path(path.package("swirl"), course.start)
+      
+      # Create master module list and get element number of starting module
+      modules <- dir(course.dir, pattern="[a-zA-Z]+[0-9]+\\.rda")
+      master.module.list <- gsub(".rda", "", as.list(modules))
+      mod.num <- which(master.module.list==module.start)
+      
+      # Start running modules beginning with starting module
+      for(i in mod.num:length(master.module.list)) {
+        # Run module i
+        module.name <- master.module.list[[i]]
+        runModule(courseDir=course.dir, module.name=module.name, 
+                  row.start=row.start, progress.file.path=progress.file.name, 
+                  courseName=courseName)
+        
+        # Suggest topics to review
+        if(file.exists(progress.file.name)) {
+          taggedTopics <- findTroubleTags(progressFilePath=progress.file.name)
+          if(!identical(taggedTopics, NA)) {
+            cat("\nIt appears that you struggled with the following topics:",
+                paste(taggedTopics, collapse=", "), "\n")
+            cat("\nWhich of these topics would you like to review?\n")
+            options <- c(taggedTopics, "I'm good to go!")
+            tags2Review <- select.list(options, multiple=TRUE)
+            
+            if(!identical(tags2Review, "I'm good to go!")) {
+              # Run module in review mode for tags of interest
+              runModule(courseDir=course.dir, module.name=module.name, 
+                        row.start=1, progress.file.path=progress.file.name,
+                        review=TRUE, tags=tags2Review)
+              cat("\nYou've completed your review for the topics you selected!\n")
+            }
           }
         }
+        
+        # Ask user if they want to continue with next module, if it exists
+        if(i < length(master.module.list)) {
+          cat("\nWould you like to continue on with the next module?")
+          continue <- readline("\nANSWER: ")
+          if(isYes(continue)) {
+            # Reset row start to 1
+            row.start <- 1
+          } else {
+            cat("\nThanks for stopping by! Your progress has been saved....\n\n")
+            break
+          }        
+        }
       }
-      
-      # Ask user if they want to continue with next module, if it exists
-      if(i < length(master.module.list)) {
-        cat("\nWould you like to continue on with the next module?")
-        continue <- readline("\nANSWER: ")
-        if(isYes(continue)) {
-          # Reset row start to 1
-          row.start <- 1
-        } else {
-          cat("\nThanks for stopping by! Your progress has been saved....\n\n")
-          break
-        }        
-      }
-    }
-  }, interrupt = function(ex) {  # If user presses Esc key
-    cat("\nThanks for stopping by! Your progress has been saved....\n\n")
-  }, error = function(ex) {  # If program stops due to error
-    cat("\nAn error was detected. Please notify the administator.\n\n")
-  }, finally = {  # No matter how program ends, close all open file connections
-    closeAllConnections()
-  })
+    }, interrupt = function(ex) {  # If user presses Esc key
+      cat("\nThanks for stopping by! Your progress has been saved....\n\n")
+    }, error = function(ex) {  # If program stops due to error
+      cat("\nAn error was detected. Please notify the administator.\n\n")
+    }, finally = {  # No matter how program ends, close all open file connections
+      closeAllConnections()
+    })
+  }
 }
--- R/updateSwirl.R
+++ R/updateSwirl.R
@@ -1,11 +1,25 @@
 #' Updates swirl package
 #' 
+#' Updates swirl package with warning that the update will 
+#' overwrite all user progress and info
+#' 
+update_swirl <- function() {
+  require(devtools)
+  cat("\nAre you sure you'd like to update swirl now and lose any progress you've made?\n\n")
+  resp <- readline("ANSWER: ")
+  if(isYes(resp)) {
+    install_github(repo="swirl", username="ncarchedi")
+  } else {
+    cat("\nNo problem! You will be prompted to update every time you start swirl. Type 'swirl()' now to restart the program.\n")
+  }
+}
+
+#' Prompts user to update
+#' 
 #' Prompts user to update swirl package with warning that the update will 
 #' overwrite all user progress and info
 #' 
-updateSwirl <- function() {
-  require(devtools)
-  
+promptUpdate <- function() {  
   cat("\nHi there! Since swirl is still young, we are constantly fixing things up and adding new content. It is recommended that you update your version at least once a month so you have full access to the latest and greatest features!\n")
   cat("\nHowever, please note that updating your version of swirl will cause you to lose your progress if you are in the middle of a course. Would you like to update swirl now?\n\n")
   resp <- readline("ANSWER: ")
@@ -14,11 +28,14 @@ updateSwirl <- function() {
     cat("\nAre you sure you'd like to update swirl now and lose any progress you've made?\n\n")
     resp <- readline("ANSWER: ")
     if(isYes(resp)) {
-      install_github(repo="swirl", username="ncarchedi")
+      cat("\nOkay! Please hit the Esc key and type 'update_swirl()'. The update may take a minute or two. Once the update is complete, type 'swirl()' and hit Enter to restart the program.\n\n")
+      return(TRUE)
     } else {
-      cat("\nNo problem! You will be prompted to update every time you start swirl.\n")
+      cat("\nNo problem! You will be prompted to update every time you start swirl.\n\n")
+      return(FALSE)
     }
   } else {
-    cat("\nNo problem! You will be prompted to update every time you start swirl.\n")
+    cat("\nNo problem! You will be prompted to update every time you start swirl.\n\n")
+    return(FALSE)
   }
 }
